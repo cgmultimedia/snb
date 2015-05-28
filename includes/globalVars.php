0acb275c238228568bf17376e7ed9af2adfdd9f9
aidanj <adamsjenkins@gmail.com>
2015-05-28 12:24:48 -0400

    Updated global vars.php

--- includes/globalVars.php
+++ includes/globalVars.php
@@ -5,10 +5,12 @@
 //$curURL = $_SERVER['REQUEST_URI'];
 $curURLA     = explode('?',$_SERVER['REQUEST_URI'],2);
 $curURL      = $curURLA[0];
-$rootDir     = $curURL."..";
-$templateDir = get_template_directory_uri();
-$rootDir     = "http://10.0.1.4:8888/web%20dev/5-splashnboots/2015/2015-02-10%20new%20site/splashnboots";
-$templateDir = "http://10.0.1.4:8888/web%20dev/5-splashnboots/2015/2015-02-10%20new%20site/splashnboots/wp-content/themes/splashnboots";
+//$rootDir     = $curURL."..";
+//$templateDir = get_template_directory_uri();
+//$rootDir     = "http://10.0.1.4:8888/web%20dev/5-splashnboots/2015/2015-02-10%20new%20site/splashnboots";
+//$templateDir = "http://10.0.1.4:8888/web%20dev/5-splashnboots/2015/2015-02-10%20new%20site/splashnboots/wp-content/themes/splashnboots";
+$rootDir     = site_url();
+$templateDir = get_stylesheet_directory_uri();
 // $foundPos   = strpos($templateDir, "/wp-content");
 // $templateDir = ".".substr($foundPos);
 
